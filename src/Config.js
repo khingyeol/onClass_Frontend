02177d5190c22dabc01e0dd1dff539070036cead
p.jonganurak <p.jonganurak@accenture.com>
2022-12-20 01:21:11 +0700

    [SP1] change build

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 let config = {
-    serviceUrl: "http://localhost:8080",
+    serviceUrl: "http://ec2-54-255-229-73.ap-southeast-1.compute.amazonaws.com:8080",
     cognitoUserPoolId: "ap-southeast-1_nOdb9Fgh5",
     cognitoClientId: "3qdarkr0anhqhfo916utptakeo",
 }
